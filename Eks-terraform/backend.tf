a31a8815f00be52bbb9977e14278ae1b81331eaa
MOHANBV153 <143169341+MOHANBV153@users.noreply.github.com>
2023-11-30 21:39:41 +0530

    Update backend.tf
    Updated the bucket name

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "s3bucket-monish" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
